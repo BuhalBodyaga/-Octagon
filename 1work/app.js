e0832c3102cacdbf2c0bb856d98d787306e64655
Богдан <119935325+BuhalBodyaga@users.noreply.github.com>
2024-07-24 16:38:41 +1000

    Update app.js

--- 1work/app.js
+++ 1work/app.js
@@ -1,21 +1,22 @@
 const express = require("express");
 
 const app = express();
-app.get("/", function (_, response) {
 
+app.get("/", function (_, response) {
     response.send("<h1>Главная страница</h1>");
 });
-app.use("/static",function (request, response) {
-    response.json({ header: "Hello", body: "Octagon NodeJS Test" });
-})
-app.use("/dynamic", function (request, response) {
-    const a = parseFloat(request.query.a);
-    const b = parseFloat(request.query.b);
-    const c = parseFloat(request.query.c);
 
-    if (isNaN(a) || isNaN(b) || isNaN(c)) {
+app.use("/static", function (request, response) {
+    response.json({ header: "Hello", body: "Octagon NodeJS Test" });
+});
+
+app.use("/dynamic", function (request, response) {
+    const params = ['a', 'b', 'c'].map(key => parseFloat(request.query[key]));
+
+    if (params.some(isNaN)) {
         response.json({ header: "Error" });
     } else {
+        const [a, b, c] = params;
         const result = (a * b * c) / 3;
         response.json({ header: "Calculated", body: result.toString() });
     }
